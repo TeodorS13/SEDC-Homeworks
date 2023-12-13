564b4e32d35d4165e8ccd7a68729c54fc4828c85
TeodorS13 <147415450+TeodorS13@users.noreply.github.com>
2023-12-13 16:42:25 +0100

    Add files via upload

---
+++ JSClass03/script.js
@@ -0,0 +1,73 @@
+// The Age Calculator
+
+// Function to calculate age
+function calculateAge(birthYear) {
+    // Get the current year dynamically
+    var currentYear = new Date().getFullYear();
+
+    // Calculate the age
+    var age = currentYear - birthYear;
+
+    // Display the result
+    console.log("You are " + age + " years old");
+}
+
+// Call the function with different sets of values
+calculateAge(1962);
+calculateAge(1915);
+calculateAge(2006);
+
+
+// Homework Part 2
+
+// Function to calculate dog's age in human years
+function calculateDogAgeInHumanYears(dogAge) {
+  let humanAge = dogAge * 7;
+  return humanAge;
+}
+
+// Function to calculate human age in dog years
+function calculateHumanAgeInDogYears(humanAge) {
+  let dogAge = humanAge / 7;
+  return dogAge;
+}
+
+// Example usage
+console.log(calculateDogAgeInHumanYears(11));  // Replace 3 with your dog's age
+console.log(calculateHumanAgeInDogYears(42)); // Replace 21 with your age
+
+
+
+// Homework Part 3
+
+// Hardcoded account balance
+let accountBalance = 1000;
+
+// Function to withdraw cash from the ATM
+function withdrawCash(amount) {
+    if (amount > accountBalance) {
+        return "Not enough money";
+    } else {
+        accountBalance -= amount;
+        return `Amount withdrawn: ${amount}, Money left on the account: ${accountBalance}`;
+    }
+}
+
+// Bonus: Make it work with prompt
+
+function withdrawWithPrompt() {
+    // Prompt the user for the withdrawal amount
+    const withdrawalAmount = parseFloat(prompt("Enter the amount to withdraw:"));
+
+    // Check if the input is a valid number
+    if (isNaN(withdrawalAmount)) {
+        alert("Invalid input. Please enter a valid number.");
+        return;
+    }
+
+    // Call the withdrawCash function and display the result
+    const result = withdrawCash(withdrawalAmount);
+    alert(result);
+}
+
+withdrawWithPrompt();
